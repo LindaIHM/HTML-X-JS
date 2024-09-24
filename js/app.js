c42cea818b8f5dc326b02047449a5330d5ef46b5
LindaIHM <lincal524@ihmeducation.se>
2024-09-24 09:46:02 +0200

    Add Button That Increases Number

--- js/app.js
+++ js/app.js
@@ -1,33 +1,35 @@
 //Documents Elements
 // Looks for "text0". Assigns that to the variable called textField0.
 const textField0 = document.getElementById("text0");
-const textField1 = document.getElementById("text1");
+
+//Buttons
+const increaceScoreButton = document.getElementById("button0");
 
 //Internal Variables
 let score = 0;
 
 // Process (What is going to happen in what sequence?)
-
-increaseScoreByOne();
+ increaceScoreButton.addEventListener("click",() => {
+  increaseScoreByOne();
+  updateScoreText();
+  checkScoreForSeven();
+ });
 
 // Controllers
-function showScore(inputNumber) {
- return inputNumber * 100; // Output is a number.
-}
-
-function showStylizedScore(scoreInput) {
- return "Your score is: " + scoreInput * 100;
-}
-
 function increaseScoreByOne() {
  score++;
- updateScoreText(score);
+}
+
+function checkScoreForSeven() {
+   if (score >= 7) {
+     changeScoreTextColorToGreen();
+   }
 }
 
 // View
-function updateScoreText(newText) {
- textField0.innerHTML = newText;
+function updateScoreText() {
+ textField0.innerHTML = "Your score is: " + score;
 }
- function updateStatusText(newText) {
-  textField1.innerHTML = newText;
- }
+function changeScoreTextColorToGreen() {
+  textField0.style.color = "green";
+}
